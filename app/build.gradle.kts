4a113e96634b4925269c7481650d935ab77ba6ac
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2025-11-20 13:40:29 -0300

    feat: Add Material Icons Extended dependency
    - Add the `material-icons-extended` dependency to `app/build.gradle.kts` for use in Compose UI.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
     implementation(libs.androidx.room.runtime)
